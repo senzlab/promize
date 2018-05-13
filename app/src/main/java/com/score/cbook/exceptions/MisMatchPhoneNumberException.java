0e5641b04da343e7a03b4b6e2459b5f3895de151
maurakirinathan <maurakiri@gmail.com>
2018-05-13 14:19:55 +0530

    added excepitons for phone number

---
+++ app/src/main/java/com/score/cbook/exceptions/MisMatchPhoneNumberException.java
@@ -0,0 +1,15 @@
+package com.score.cbook.exceptions;
+
+/**
+ * Created by mauran on 5/13/18.
+ */
+
+public class MisMatchPhoneNumberException extends Exception {
+
+    @Override
+    public String toString() {
+        return "phone mismatch";
+    }
+
+}
+
